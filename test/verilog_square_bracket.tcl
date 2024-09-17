df04f090d5546e2ee8b56e852de77e7048381395
Akash Levy <akashlevy@gmail.com>
2024-09-17 14:37:50 -0700

    Conform to testing guidelines

--- test/verilog_square_bracket.tcl
+++ test/verilog_square_bracket.tcl
@@ -1,4 +1,4 @@
-# Read in design and libraries
+# Verilog input pin with square brackets in middle of pin name
 read_liberty asap7_small.lib.gz
 read_verilog verilog_square_bracket.v
 link_design square_bracket
