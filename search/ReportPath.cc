cdf069807000c6e4e3d28dbc35ea486cb438a001
Akash Levy <akash@silimate.com>
2024-11-10 17:54:50 -0800

    Any liberty cell with macro/memory/seq should report cell name

--- search/ReportPath.cc
+++ search/ReportPath.cc
@@ -2850,8 +2850,9 @@ ReportPath::descriptionField(Vertex *vertex)
   }
   else {
     Instance *inst = network_->instance(pin);
+    LibertyCell *libcell = network_->libertyCell(inst);
     name2 = network_->cellName(inst);
-    if (network_->portName(pin)[0] == 'D' && stringEq(network_->name(inst) + strlen(network_->name(inst)) - 4, "_reg"))
+    if (libcell && (libcell->isMemory() || libcell->isMacro() || libcell->hasSequentials()))
       pin_name = network_->pathName(inst);
     if (network_->getAttribute(inst, "src") != "") {
       return stdstrPrint("%s (%s) @ %s", pin_name, name2, network_->getAttribute(inst, "src").c_str());
