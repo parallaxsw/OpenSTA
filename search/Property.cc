3b7bf8407b9a9b21178927b4588f0da6b1ae6fd2
Akash Levy <akash@silimate.com>
2024-11-20 15:08:36 -0800

    Remove is_memory_cell

--- search/Property.cc
+++ search/Property.cc
@@ -726,8 +726,7 @@ getProperty(const LibertyCell *cell,
     return PropertyValue(cell->isBuffer());
   else if (stringEqual(property, "is_inverter"))
     return PropertyValue(cell->isInverter());
-  else if (stringEqual(property, "is_memory")
-	   || stringEqual(property, "is_memory_cell"))
+  else if (stringEqual(property, "is_memory"))
     return PropertyValue(cell->isMemory());
   else if (stringEqual(property, "dont_use"))
     return PropertyValue(cell->dontUse());
@@ -960,8 +959,7 @@ getProperty(const Instance *inst,
     return PropertyValue(liberty_cell && liberty_cell->isInverter());
   else if (stringEqual(property, "is_macro"))
     return PropertyValue(liberty_cell && liberty_cell->isMacro());
-  else if (stringEqual(property, "is_memory")
-	   || stringEqual(property, "is_memory_cell"))
+  else if (stringEqual(property, "is_memory"))
     return PropertyValue(liberty_cell && liberty_cell->isMemory());
   else
     throw PropertyUnknown("instance", property);
