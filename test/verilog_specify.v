27ef3adf15dbabe1095cc8c45c421bf0ef921906
andrey <203089955+ambd161@users.noreply.github.com>
2025-10-08 11:38:51 -0700

    Apply PR feedback

--- test/verilog_specify.v
+++ test/verilog_specify.v
@@ -1,5 +1,4 @@
 
-(* src = "synthesis/tests/counter.v:16.1-32.10" *)
 module counter(clk, reset, in, out);
   input clk;
   output out;
@@ -17,17 +16,5 @@ module counter(clk, reset, in, out);
 
   defparam _1415_.PARAM2 = 1;
 
-  sky130_fd_sc_hd__dfrtp_1 _1415_ (
-    .CLK(clk),
-    .D(in),
-    .Q(mid),
-    .RESET_B(reset)
-  );
 
-  sky130_fd_sc_hd__dfrtp_1 \_1416_[0] (
-    .CLK(clk),
-    .D(mid),
-    .Q(out),
-    .RESET_B(reset)
-  );
 endmodule
