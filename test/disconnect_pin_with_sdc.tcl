3648c33cc29f055f27a16a9964ef92f1cdffdc1e
Jaehyun Kim <jhkim@precisioninno.com>
2025-11-11 10:52:11 +0900

    Revised the test case to show anchor buffers on a source-synchronous interafce
    Signed-off-by: Jaehyun Kim <jhkim@precisioninno.com>

--- test/disconnect_pin_with_sdc.tcl
+++ test/disconnect_pin_with_sdc.tcl
@@ -1,26 +1,24 @@
 # disconnect_pin call for a pin where SDC is defined
-read_liberty asap7_invbuf.lib.gz
-read_liberty asap7_seq.lib.gz
-read_liberty asap7_simple.lib.gz
+read_liberty asap7_small.lib.gz
 read_verilog disconnect_pin_with_sdc.v
 link_design top
 
-create_clock -name clk -period 500 {clk1 clk2 clk3}
-set_input_delay -clock clk 1 {in1 in2}
-set_input_transition 10 {in1 in2 clk1 clk2 clk3}
-set_propagated_clock {clk1 clk2 clk3}
-sta::set_delay_calculator prima
+create_clock -name clk -period 500 clk
 
 #sta::set_debug exception_merge 10
 
-puts "set_multicycle_path on pins"
-set_multicycle_path -end   -setup 1 -to [get_pins {u0/buf0/A}]
-set_multicycle_path -end   -setup 1 -to [get_pins {u0/buf1/A}]
-set_multicycle_path -end   -setup 1 -to [get_pins {u0/buf2/A}]
-set_multicycle_path -end   -setup 1 -to [get_pins {u0/buf3/A}]
+# This SDC defines setup and hold time requirements for data pins
+# relative to a clock, typical for a source-synchronous interface.
+set_data_check -from clk -to [get_pins u0/A] -setup 10
+set_data_check -from clk -to [get_pins u0/A] -hold  10
+set_data_check -from clk -to [get_pins u1/A] -setup 10
+set_data_check -from clk -to [get_pins u1/A] -hold  10
+set_multicycle_path -end   -setup 1 -to [get_pins u0/A]
+set_multicycle_path -end   -setup 1 -to [get_pins u1/A]
+set_multicycle_path -start -hold  0 -to [get_pins u0/A]
+set_multicycle_path -start -hold  0 -to [get_pins u1/A]
 
-puts "disconnect the pin w/ SDC"
-set in_pin [get_pins u0/buf2/A]
+set in_pin [get_pins u1/A]
 set in_net [get_net -of $in_pin]
 disconnect_pin $in_net $in_pin
 
--- test/disconnect_pin_with_sdc.v
+++ test/disconnect_pin_with_sdc.v
@@ -1,47 +1,11 @@
-module top (in1, in2, clk1, clk2, clk3, out);
-  input in1, in2, clk1, clk2, clk3;
-  output out;
-  wire r1q, r2q, u1z, u2z;
-
-  DFFHQx4_ASAP7_75t_R r1 (.D(in1), .CLK(clk1), .Q(r1q));
-  DFFHQx4_ASAP7_75t_R r2 (.D(in2), .CLK(clk2), .Q(r2q));
-  BUFx2_ASAP7_75t_R u1 (.A(r2q), .Y(u1z));
-  AND2x2_ASAP7_75t_R u2 (.A(r1q), .B(u1z), .Y(u2z));
-  DFFHQx4_ASAP7_75t_R r3 (.D(u2z), .CLK(clk3), .Q(out));
-
-  test_mod u0 (.clk(clk1), .A({u1z, u1z, u1z, u1z}));
-
-endmodule // top
-
-
-module test_mod(clk, clkout, A, B);
+module top (clk, clkout, data_in, data_out);
   input clk;
   output clkout;
-  input [3:0] A;
-  output [3:0] B;
+  input [1:0] data_in;
+  output [1:0] data_out;
 
-  wire w0;
-  wire w1;
-  wire w2;
-  wire w3;
-
- BUFx4_ASAP7_75t_R clkbuf0 (.A(clk),
-    .Y(clkout));
- BUFx4_ASAP7_75t_R buf0 (.A(A[0]),
-    .Y(w0));
- BUFx4_ASAP7_75t_R buf0_1 (.A(w0),
-    .Y(B[0]));
- BUFx4_ASAP7_75t_R buf1 (.A(A[1]),
-    .Y(w1));    
- BUFx4_ASAP7_75t_R buf1_1 (.A(w1),
-    .Y(B[1]));
- BUFx4_ASAP7_75t_R buf2 (.A(A[2]),
-    .Y(w2));
- BUFx4_ASAP7_75t_R buf2_1 (.A(w2),
-    .Y(B[2]));
- BUFx4_ASAP7_75t_R buf3 (.A(A[3]),
-    .Y(w3));    
- BUFx4_ASAP7_75t_R buf3_1 (.A(w3),
-    .Y(B[3]));
-
-endmodule
+  // Anchor buffers on the source-synchronous interface IOs
+  BUFx2_ASAP7_75t_R clkbuf0 (.A(clk), .Y(clkout));
+  BUFx2_ASAP7_75t_R u0 (.A(data_in[0]), .Y(data_out[0]));
+  BUFx2_ASAP7_75t_R u1 (.A(data_in[1]), .Y(data_out[1]));
+endmodule // top
