2258beb78a65428d807311ca6354b21304dca2f4
Jaehyun Kim <jhkim@precisioninno.com>
2025-11-12 01:14:45 +0900

    Removed redundant codes
    Signed-off-by: Jaehyun Kim <jhkim@precisioninno.com>

--- test/disconnect_pin_with_sdc.tcl
+++ test/disconnect_pin_with_sdc.tcl
@@ -5,8 +5,6 @@ link_design top
 
 create_clock -name clk -period 500 clk
 
-#sta::set_debug exception_merge 10
-
 # This SDC defines setup and hold time requirements for data pins
 # relative to a clock, typical for a source-synchronous interface.
 set_data_check -from clk -to [get_pins u0/A] -setup 10
@@ -21,5 +19,3 @@ set_multicycle_path -start -hold  0 -to [get_pins u1/A]
 set in_pin [get_pins u1/A]
 set in_net [get_net -of $in_pin]
 disconnect_pin $in_net $in_pin
-
-puts "Pass"
