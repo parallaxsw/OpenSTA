1e3debc284745eb9d4635aff3469d3a2c83b7cc3
dsengupta0628 <dsengupta@precisioninno.com>
2026-05-20 10:39:03 +0000

    reduce test verbosity
    Signed-off-by: dsengupta0628 <dsengupta@precisioninno.com>

--- test/make_concrete_parasitics_leak.tcl
+++ test/make_concrete_parasitics_leak.tcl
@@ -1,7 +1,4 @@
 # Sta::makeConcreteParasitics map-overwrite leak repro.
-# Two read_spef -min calls share parasitics_name_map_ key "default_min".
-# Without the reuse-and-clear guard in Sta::makeConcreteParasitics the
-# second call orphans and leaks the first ConcreteParasitics.
 # Run under -fsanitize=address to verify no leak after the fix.
 read_liberty asap7_small.lib.gz
 read_verilog reg1_asap7.v
