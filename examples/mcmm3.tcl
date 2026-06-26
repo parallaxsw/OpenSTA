ddc6e3f25998bd165faa9f1003980d1ef038841c
dsengupta0628 <dsengupta@precisioninno.com>
2026-06-26 16:55:55 +0000

    make mode/scene pass as tcl objects
    Signed-off-by: dsengupta0628 <dsengupta@precisioninno.com>

--- examples/mcmm3.tcl
+++ examples/mcmm3.tcl
@@ -16,3 +16,22 @@ define_scene scene2 -mode mode2 -liberty asap7_small_ss -spef reg1_ss
 report_checks -scenes scene1
 report_checks -scenes scene2
 report_checks -group_path_count 4
+
+# scenes/modes cross to Tcl as objects; mode/scene counts stay 2.
+puts "--- scene/mode object coverage ---"
+puts "num modes         : [llength [get_modes *]]"
+puts "num scenes        : [llength [get_scenes]]"
+puts "cmd_scene type    : [sta::object_type [sta::cmd_scene]]"
+puts "cmd_mode_name     : [sta::cmd_mode_name]"
+foreach mode [get_modes *] {
+  puts "get_modes type    : [sta::object_type $mode]"
+}
+foreach scene [get_scenes] {
+  puts "get_scenes type   : [sta::object_type $scene]"
+}
+set_scene scene2
+puts "set_scene scene2 -> cmd_mode_name : [sta::cmd_mode_name]"
+set_scene scene1
+puts "set_scene scene1 -> cmd_mode_name : [sta::cmd_mode_name]"
+# scene objects (not just names) flow through report_checks.
+report_checks -scenes [get_scenes scene2]
